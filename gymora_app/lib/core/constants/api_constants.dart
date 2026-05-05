fe7785dc8a270aaccc78e9f0f269338d4ebd75fe
Ayush Vishwakarma <ayushvishwakarmadto29@gmail.com>
2026-05-05 19:14:54 +0530

    Switch to stable LocalTunnel URL for backend

--- gymora_app/lib/core/constants/api_constants.dart
+++ gymora_app/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://10.147.25.216:8080/api';
+  static const String baseUrl = 'https://violet-chicken-check.loca.lt/api';
 
   // Auth
   static const String login = '/auth/login';
