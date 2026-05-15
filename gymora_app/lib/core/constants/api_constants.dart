63f356bcbbc787b6bd953d8b2088e39c6c449baa
Ayush Vishwakarma <ayushvishwakarmadto29@gmail.com>
2026-05-15 13:53:29 +0530

    🌐 Fix connectivity: Update baseUrl to PC local IP (10.99.152.216) for real device testing

--- gymora_app/lib/core/constants/api_constants.dart
+++ gymora_app/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://localhost:8080/api';
+  static const String baseUrl = 'http://10.99.152.216:8080/api';
 
   // Auth
   static const String login = '/auth/login';
